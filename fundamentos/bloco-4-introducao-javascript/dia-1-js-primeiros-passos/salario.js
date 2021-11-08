e15f99641a9eeaa80fa6cba44a89442e7482ec01
Andrade <italoandrade13@gmail.com>
2021-11-07 23:03:19 -0300

    Fixed error in reward

--- fundamentos/bloco-4-introducao-javascript/dia-1-js-primeiros-passos/salario.js
+++ fundamentos/bloco-4-introducao-javascript/dia-1-js-primeiros-passos/salario.js
@@ -1,39 +1,32 @@
-var salario = 3000
-var desconto
+let salario = 3000
 var leao 
 var salario_base
 
+
 if (salario < 1556.94) {
-    desconto = salario * 0.08 
+    salario_base = salario * (1 - 0.08); 
 }
 else if (salario > 1556.94 && salario < 2594.93) {
-    desconto = salario * 0.09 
+    salario_base = salario * (1 - 0.09);
 }
 else if (salario > 2594.92 && salario < 5189.83)  {
-    desconto = salario * 0.11 
+    salario_base = salario * (1 - 0.11); 
 }
 else if (salario > 5189.82) {
-    desconto = 570.88
+    salario_base = salario - 570.88;
 }
-salario_base = salario - desconto
+
 
 if (salario_base <= 1903.98) {
-    leao = 0
+        leao = 0
+}else if (salario_base > 1903.98 && salario_base  <= 2826.65) {
+    leao = salario_base * 0.075 - 142.80
+}else if (salario_base > 2826.65 && salario_base  <=3751.05) {
+    leao = salario_base * 0.15 - 354.80
+}else if (salario_base > 3751.05 && salario_base  <=4664.68) {
+    leao = salario_base * 0.225 - 636.13
+}else if (salario_base > 4664.68) {
+    leao = salario_base * 0.275 - 869.36
 }
-
-else if (salario_base > 1903.98 && salario <=2826.65) {
-    leao = salario_base * 0.075
-}
-
-else if (salario_base > 2826.65 && salario <=3751.05) {
-    leao = salario_base * 0.15
-}
-else if (salario_base > 3751.05 && salario <=4664.68) {
-    leao = salario_base * 0.225
-}
-else if (salario_base > 4664.68) {
-    leao = salario_base * 0.275
-}
-var liq = salario_base - leao
-
-console.log(leao)
+let liq = salario_base - leao
+console.log(liq)
