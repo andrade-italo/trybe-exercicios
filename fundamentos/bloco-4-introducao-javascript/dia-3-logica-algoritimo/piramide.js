3d7a834f58d9f6a47d63624b11374c485be204e9
Andrade <italoandrade13@gmail.com>
2021-11-09 20:32:33 -0300

    Meus exercicios do bloco 4, aula 3. Algumas correçoes em exercicios anteriores tambem

---
+++ fundamentos/bloco-4-introducao-javascript/dia-3-logica-algoritimo/piramide.js
@@ -0,0 +1,11 @@
+const n = 6
+let cont = 1
+var asterisco = '*'
+var espace = ' '
+var x = n
+while (n >= cont) {
+    x -= 1
+    console.log(espace.repeat(x) + asterisco.repeat(cont))
+    cont+=2
+    
+}
---
+++ fundamentos/bloco-4-introducao-javascript/dia-3-logica-algoritimo/quadrado-de-asterisco.js
@@ -0,0 +1,9 @@
+const n = 2;
+const list = '*';
+var cont = 0;
+
+
+while (n > cont) {
+    cont+=1;
+    console.log(list.repeat(n))    
+}
