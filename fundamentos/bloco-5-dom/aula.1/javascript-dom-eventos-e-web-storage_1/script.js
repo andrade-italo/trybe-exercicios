0ed36f359eac66bb9dd1d3733e40f1602f4564a7
Andrade <italoandrade13@gmail.com>
2021-11-17 14:47:56 -0300

    adc cor de fundo branco

--- fundamentos/bloco-5-dom/aula.1/javascript-dom-eventos-e-web-storage_1/script.js
+++ fundamentos/bloco-5-dom/aula.1/javascript-dom-eventos-e-web-storage_1/script.js
@@ -7,6 +7,12 @@
 
  // 01. Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
 let p2 = document.getElementsByTagName("p")[1].innerText = 'Em 2 anos quero trazer algo inovador'
+
 // 02. Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
 let quadradoAmarelo = document.getElementsByClassName("main-content")[0]
 quadradoAmarelo.style.background = "rgb(76,164,109)" 
+
+// 03. Crie uma função que mude a cor do quadrado vermelho para branco.
+let quadradoVermelho = document.getElementsByClassName("center-content")[0]
+quadradoVermelho.style.background = "white" 
+
