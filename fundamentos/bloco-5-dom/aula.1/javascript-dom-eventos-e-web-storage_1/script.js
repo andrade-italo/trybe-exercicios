514fa17bfb9a0c0e61ad9d4b3dc97db698737fa6
Andrade <italoandrade13@gmail.com>
2021-11-17 15:11:30 -0300

    correçao de identaçao

--- fundamentos/bloco-5-dom/aula.1/javascript-dom-eventos-e-web-storage_1/script.js
+++ fundamentos/bloco-5-dom/aula.1/javascript-dom-eventos-e-web-storage_1/script.js
@@ -1,20 +1,20 @@
-  /*
-        Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
-        - document.getElementById()
-        - document.getElementsByClassName()
-        - document.getElementsByTagName()
-         */
+/*
+      Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
+      - document.getElementById()
+      - document.getElementsByClassName()
+      - document.getElementsByTagName()
+       */
 
- // 01. Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+// 01. Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
 let p2 = document.getElementsByTagName("p")[1].innerText = 'Em 2 anos quero trazer algo inovador'
 
 // 02. Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
 let quadradoAmarelo = document.getElementsByClassName("main-content")[0]
-quadradoAmarelo.style.background = "rgb(76,164,109)" 
+quadradoAmarelo.style.background = "rgb(76,164,109)"
 
 // 03. Crie uma função que mude a cor do quadrado vermelho para branco.
 let quadradoVermelho = document.getElementsByClassName("center-content")[0]
-quadradoVermelho.style.background = "white" 
+quadradoVermelho.style.background = "white"
 
 // 04. Crie uma função que corrija o texto da tag <h1>.
 let h1 = document.getElementsByClassName("title")[0]
