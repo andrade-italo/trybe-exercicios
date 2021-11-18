7e3a75ec3c30e502ea0118efd776d3941699bb89
Andrade <italoandrade13@gmail.com>
2021-11-17 21:04:08 -0300

    Adc fixando aprendizado 2 e suas estruturas pre exercicio

---
+++ fundamentos/bloco-5-dom/aula.2/fixandoAprendizado/script2.js
@@ -0,0 +1,7 @@
+// 01.Crie um irmão para elementoOndeVoceEsta .
+
+// 02.Crie um filho para elementoOndeVoceEsta .
+
+// 03.Crie um filho para primeiroFilhoDoFilho .
+
+// 04.A partir desse filho criado, acesse terceiroFilho .
