72c6e86f7ce7ea7ddfa3e46e54e0d4fee79111cd
Italo Andrade <italoandrade13@gmail.com>
2022-02-03 21:29:27 -0300

    Exercicios do dia

---
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/dia-1-ciclo-de-vida-de-componentes/fixando-aprendizado/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import DadJoke from "./DadJoke";
+import Counter from "./Counter";
+
+function App() {
+  return (
+    <h1>
+      <DadJoke />
+      <Counter />
+    </h1>
+  );
+}
+
+export default App;
