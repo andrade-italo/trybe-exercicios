74deeb7277c5df2e15338c417612669e68985fbb
Italo Andrade <italoandrade13@gmail.com>
2022-05-13 20:46:38 -0300

    Feat: Crie uma rota POST /greetings

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicio/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicio/index.js
@@ -13,6 +13,13 @@ app.post("/hello", function (req, res) {
   res.status(201).json({ message: `Hello, ${name}` });
 });
 
+app.post("/greetings", function (req, res) {
+    const { name, age } = req.body;
+    if (age < 17) return res.status(401).json({ message: `Unauthorized` });
+  res.status(200).json({ message: `Hello, ${name}` });
+});
+
+
 app.listen(3001, () => {
   console.log("Aplicação ouvindo na porta 3001");
 });
