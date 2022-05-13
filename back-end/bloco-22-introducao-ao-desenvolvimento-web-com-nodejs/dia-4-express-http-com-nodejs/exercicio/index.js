17f1472dcbba22a9aed9b2f271fc948ba4836b46
Italo Andrade <italoandrade13@gmail.com>
2022-05-13 20:09:47 -0300

    Feat: Crie uma rota POST /hello

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicio/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicio/index.js
@@ -1,10 +1,18 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.json());
 
-app.get('/ping', function (req, res) {
-    res.status(200).json({ message: 'Pong'});
-  });
+app.get("/ping", function (req, res) {
+  res.status(200).json({ message: "Pong" });
+});
 
-  app.listen(3001, () => {
-    console.log('Aplicação ouvindo na porta 3001');
-  });
+app.post("/hello", function (req, res) {
+    const { name } = req.body;
+  res.status(201).json({ message: `Hello, ${name}` });
+});
+
+app.listen(3001, () => {
+  console.log("Aplicação ouvindo na porta 3001");
+});
