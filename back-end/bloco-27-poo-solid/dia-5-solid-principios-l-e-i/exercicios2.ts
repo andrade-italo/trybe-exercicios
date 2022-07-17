c50a50cfbed0e4c6fcd8caa6fb5f4e12926a68ce
Italo Andrade <italoandrade13@gmail.com>
2022-07-17 16:22:37 -0300

    exercicios

---
+++ back-end/bloco-27-poo-solid/dia-5-solid-principios-l-e-i/exercicios2.ts
@@ -0,0 +1,44 @@
+interface IFooCepApi {
+  getAddressByCEP(cep: string, number: number): Promise<string>
+  getCepByAddress(address: string, number: number): Promise<string>
+}
+
+class FooCepAPI implements IFooCepApi {
+  async getAddressByCEP(cep: string, number: number): Promise<string> {
+    return `O endereço para o "CEP:${cep}, n°:${number}" é "endereço foo"`;
+  }
+
+  async getCepByAddress(address: string, number: number): Promise<string> {
+    return `O CEP para: "${address}, n°: ${number}" é "CEP baz"`;
+  }
+}
+
+class CepService {
+  constructor(private readonly cepApi: IFooCepApi = new FooCepAPI()) {}
+
+  addressByCep(cep: string, num: number) {
+    return this.cepApi.getAddressByCEP(cep, num);
+  }
+
+  cepByAddress(address: string, num: number) {
+    return this.cepApi.getCepByAddress(address, num);
+  }
+}
+
+
+async function main() {
+  const cepSvc = new CepService(new FooCepAPI());
+
+  console.log(
+    'get address by cep', 
+    '->', 
+    await cepSvc.addressByCep('xx.xxx-xx', 10),
+  );
+  console.log(
+    'get cep by address', 
+    '->', 
+    await cepSvc.cepByAddress('street foo, between bar and baz', 10),
+  );
+}
+
+main();
